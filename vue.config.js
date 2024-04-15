7ff3fa07e578b6ab269868bf1d0e4e80f1c5291e
Qkfw <fwqaaq@gmail.com>
2024-04-15 16:39:40 +0800

    fix publicPath

--- vue.config.js
+++ vue.config.js
@@ -15,8 +15,6 @@ const name = defaultSettings.title || 'vue Element Admin' // page title
 // port = 9527 npm run dev OR npm run dev --port = 9527
 const port = process.env.port || process.env.npm_config_port || 9527 // dev port
 
-console.log(process.env.NODE_ENV)
-
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
   /**
